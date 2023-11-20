b24d65d3313ae965e7b4389b3b35ac8a0e9bd2cb
Danielle Rossetti <daniellecrossetti@gmail.com>
2023-11-20 13:06:10 -0800

    front end stuff

--- density_map.js
+++ density_map.js
@@ -1,6 +1,7 @@
 
 // initialize Leaflet
-var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+var map = L.map('map').setView({lon: -98.230850, lat: 38.938048}, 4.5);
+
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,4 +13,40 @@ attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap
 L.control.scale({imperial: true, metric: true}).addTo(map);
 
 // show a marker on the map
-L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+//L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+
+/*
+L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+L.control.textbox({ position: 'bottomleft' }).addTo(map);
+*/
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'legendbox'),
+    grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+    labels = [];
+
+    div.innerHTML = "Legend<br>"
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}  
+
+legend.addTo(map);
