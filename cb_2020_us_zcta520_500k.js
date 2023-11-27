957e17b93b60dbc8dec1086f3d9846ab15cce58a
Alison <seoyunalison@gmail.com>
2023-11-27 11:02:31 -0800

    this is gonna work

---
+++ cb_2020_us_zcta520_500k.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c1df1d98e415725086e461c9052a385b7e4dfc25d8f788056740fe03bf7af7e
+size 145417181
--- map_data.py
+++ map_data.py
@@ -5,6 +5,7 @@ Nov 2023
 
 # creating US Census API query for the population of each ZCTA within the 2020 Decennial Census
 import requests 
+import json
 
 host = 'https://api.census.gov/data'
 year = '/2020'
@@ -17,6 +18,26 @@ usr_key = "&key=52eb42bcce51db27538d66ad91d4ceb476f37c47" #probably should store
 query_url = f"{host}{year}{dataset_acronym}{g}{variables}{location}{usr_key}"
 response = requests.get(query_url)
 response_json = response.json()
+
+#Take this list of lists.... create each entry where {P1_001N : ....etc.}
+
+all_ZCTA_json = {}
+
+for j in range(1, (len(response_json))):
+    new_entry = {}
+    for i in range(len(response_json[0])):
+        each_parameter = response_json[0][i]
+        new_entry[each_parameter] = response_json[j][i]
+    new_entry_json = json.dumps(new_entry)
+    all_ZCTA_json[str(j)] = new_entry_json
+
+#print(all_ZCTA_json)
+
+json_object = json.dumps(all_ZCTA_json) 
+with open("ZCTA_population_data.json", "w") as outfile:
+    outfile.write(json_object)
+   
+
 
 
 """
